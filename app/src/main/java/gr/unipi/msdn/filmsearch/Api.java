be846007196d796b012818cc100a9f9f2b4314af
Fordakias <alexrafalexadridis@gmail.com>
2018-09-24 20:12:39 +0300

    Get and Read Json

--- app/src/main/java/gr/unipi/msdn/filmsearch/Api.java
+++ app/src/main/java/gr/unipi/msdn/filmsearch/Api.java
@@ -10,5 +10,5 @@ public interface Api {
     String URL = "https://api.themoviedb.org/3/movie/";
 
     @GET("top_rated?api_key=f888b70bdc55df2cceefdcf5523e691b&language=en-US")
-    Call<List<MoviesDataModel>> getMovies();
+    Call<MoviesDataModel> getMovies();
 }
